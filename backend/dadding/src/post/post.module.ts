ddc9a946fbf4d81790009648fd823df918d91004
3x-haust <lyusungyun@icloud.com>
2024-09-25 09:37:47 +0900

    refactor: add app repository

--- backend/dadding/src/post/post.module.ts
+++ backend/dadding/src/post/post.module.ts
@@ -2,12 +2,29 @@ import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { FirebaseModule } from 'src/firebase/firebase.module';
-import { PostRepository } from './post.repository';
 import { ResponseStrategy } from '../shared/strategies/response.strategy';
+import { AppRepository } from 'src/app.repository';
+import { FirebaseService } from 'src/firebase/firebase.service';
+import { Post } from './entities/post.entity';
 
 @Module({
   imports: [FirebaseModule],
   controllers: [PostController],
-  providers: [PostService, PostRepository, ResponseStrategy],
+  providers: [
+    PostService,
+    ResponseStrategy,
+    {
+      provide: 'POST_COLLECTION',
+      useValue: 'post',
+    },
+    {
+      provide: 'POST_REPOSITORY',
+      useFactory: (firebaseService: FirebaseService, collection: string) => {
+        return new AppRepository<Post>(firebaseService, collection);
+      },
+      inject: [FirebaseService, 'POST_COLLECTION'],
+    },
+  ],
+  exports: [PostService],
 })
 export class PostModule {}
